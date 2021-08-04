96b15f71dbb71d56c67483e16e50738fd322d042
AlexMuia31 <alexmuia31@gmail.com>
2021-08-05 01:36:50 +0300

    material ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Header from './components/Header';
+import ButtonAppBar from './components/Header';
 import PostLoading from './components/PostLoading';
 import Posts from './components/Posts';
 import Footer from './components/Footer';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <PostLoading />
-      <Posts />
+    <Router>
+      <ButtonAppBar />
+      <div className="App">
+        <Switch>
+          <Route path='/' exact></Route>
+          <Route path='/postloading' exact><PostLoading /></Route>
+          <Route path='/posts' exact><Posts /></Route>
+        </Switch>
+      </div>
       <Footer />
-
-    </div>
+    </Router>
   );
 }
 
